f1d9b2972ae15b2374cc3fd45585d90faff9ec1e
weiyafei <2553241022@qq.com>
2023-08-11 22:19:25 +0800

    feat: 添加vitest配置文件

--- packages/basic/__test__/basic.spec.ts
+++ packages/basic/__test__/basic.spec.ts
@@ -2,11 +2,9 @@
  * @Description:
  * @Author: wsy
  * @Date: 2023-08-11 22:10:54
- * @LastEditTime: 2023-08-11 22:14:28
+ * @LastEditTime: 2023-08-11 22:18:34
  * @LastEditors: wsy
  */
-import { expect, it } from 'vitest'
-
 it('description', async () => {
   expect(1).toBe(1)
 })
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+/*
+ * @Description:
+ * @Author: wsy
+ * @Date: 2023-08-11 22:17:55
+ * @LastEditTime: 2023-08-11 22:18:05
+ * @LastEditors: wsy
+ */
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    globals: true,
+  },
+})
