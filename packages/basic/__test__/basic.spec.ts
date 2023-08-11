6beeb494bff8e5fbc3bcb18c20a4188324a8b4e5
weiyafei <2553241022@qq.com>
2023-08-11 22:16:22 +0800

    feat: 添加测试vitest

---
+++ packages/basic/__test__/basic.spec.ts
@@ -0,0 +1,12 @@
+/*
+ * @Description:
+ * @Author: wsy
+ * @Date: 2023-08-11 22:10:54
+ * @LastEditTime: 2023-08-11 22:14:28
+ * @LastEditors: wsy
+ */
+import { expect, it } from 'vitest'
+
+it('description', async () => {
+  expect(1).toBe(1)
+})
