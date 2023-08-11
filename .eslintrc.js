4dbe929e86b28917d7b41144d5e125662d71fc7a
weiyafei <2553241022@qq.com>
2023-08-11 21:21:57 +0800

    feat: init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,11 @@
+/*
+ * @Description:
+ * @Author: wsy
+ * @Date: 2023-08-10 23:56:04
+ * @LastEditTime: 2023-08-11 21:18:18
+ * @LastEditors: wsy
+ */
 module.exports = {
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ["custom"],
-  settings: {
-    next: {
-      rootDir: ["apps/*/"],
-    },
-  },
-};
+  extends: ['@antfu'],
+}
