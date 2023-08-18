e234a2f0834c67ee8ee78f0485567cb565d60d35
weiyafei <2553241022@qq.com>
2023-08-18 21:43:12 +0800

    feat: 状态在database内

--- packages/basic/__test__/state.spec.ts
+++ packages/basic/__test__/state.spec.ts
@@ -1,9 +1,19 @@
-import { Counter } from '../src/state'
+import { Counter, Database, UserService } from '../src/state'
 
 describe('状态验证', () => {
   it('count', () => {
     const count = new Counter()
     count.increment()
     expect(count.getCount()).toBe(1)
+  })
+})
+
+describe('状态存在于database', () => {
+  it('should create user', async () => {
+    const database = new Database()
+    const user = new UserService(database)
+    const newUser = user.createUser('test')
+    expect(newUser.name).toBe('test')
+    expect(database.getUser()).toContainEqual(newUser)
   })
 })
--- packages/basic/src/state.ts
+++ packages/basic/src/state.ts
@@ -12,3 +12,31 @@ export class Counter {
     return this.count
   }
 }
+
+interface User {
+  id: number
+  name: string
+}
+
+export class Database {
+  private database: User[] = []
+  addUser(newUser: User) {
+    this.database.push(newUser)
+  }
+
+  getUser() {
+    return this.database
+  }
+}
+
+export class UserService {
+  constructor(private database: Database) {
+  }
+
+  createUser(name: string): User {
+    const id = Math.floor(Math.random() * 1000)
+    const newUser: User = { id, name }
+    this.database.addUser(newUser)
+    return newUser
+  }
+}
